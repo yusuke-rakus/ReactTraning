b5deb8674328925ad85c06bfd87e4dbe37dd9c74
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2022-06-18 23:23:13 +0900

    props, stateを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/colorfulMessage";
 
 const App = () => {
+  // useStateでデフォルト値を設定し、setterとタグ内のフィールド名をきめられる
+  const [num, setNum] = useState(0);
+
+  const [faceflg, setfaceflg] = useState("(ー▽ー)");
+
+  const [faceShowFlg, setFaceShowFlg] = useState(true);
+
   const clickButton = () => {
-    alert("本当に登録しますか");
+    // useStateで定義したsetter名
+    setNum(num + 1);
+    if ((num + 1) % 3 == 0) {
+      setfaceflg("ｷﾀ━━━━(ﾟ∀ﾟ)━━━━!!");
+    } else {
+      setfaceflg("(ー▽ー)");
+    }
   };
 
-  const girl = {
-    color: "red",
-    fontWeight: "bold",
+  const changeFaceButton = () => {
+    if (faceflg == "(ー▽ー)") {
+      setfaceflg("ｷﾀ━━━━(ﾟ∀ﾟ)━━━━!!");
+    } else {
+      setfaceflg("(ー▽ー)");
+    }
+  };
+
+  const setFaceShowFlgButton = () => {
+    if (faceShowFlg) {
+      setFaceShowFlg(false);
+    } else {
+      setFaceShowFlg(true);
+    }
   };
 
   return (
@@ -19,7 +43,14 @@ const App = () => {
       <ColorfulMessage selfcolor="blue" message="こんにちは" />
       <ColorfulMessage selfcolor="red" message="こんにちは" />
 
+      <p>{num}</p>
       <button onClick={clickButton}>Button</button>
+
+      <p>{faceflg}</p>
+      <button onClick={changeFaceButton}>顔文字</button>
+
+      <p>{faceShowFlg && <span>(*´ω｀*)</span>}</p>
+      <button onClick={setFaceShowFlgButton}>Button</button>
     </>
   );
 };
--- src/components/colorfulMessage.jsx
+++ src/components/colorfulMessage.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const ColorfulMessage = (props) => {
   // 変数を一括で取り込み
   const { selfcolor, message } = props;
-  const boy = {
+  const humanStyle = {
     color: selfcolor,
     // color: props.selfcolor でも可能
   };
 
-  return <p style={boy}>{message}</p>;
+  return <p style={humanStyle}>{message}</p>;
 };
 
 export default ColorfulMessage;
